f88a420c22dfb53ddc804762d91d5c6ad67868d0
linxx5599 <linxx5599@163.com>
2024-11-11 21:25:42 +0800

    rec: 重构JsonResponseFairing

--- src/mod.rs
+++ src/mod.rs
@@ -22,23 +22,29 @@ impl Fairing for JsonResponseFairing {
         match response.body_mut() {
             body => {
                 let mut body_bytes: Vec<u8> = Vec::new();
-                if let Some(data) = body.into() {
-                    data.read_to_end(&mut body_bytes).await.unwrap();
-                    let body_str = String::from_utf8(body_bytes).unwrap();
-                    let body_json: serde_json::Value = serde_json::from_str(&body_str).unwrap();
-                    let json_body = json!({ "data": body_json, "status": Status::Ok.code, "message": "操作成功" });
-                    let cursor = Cursor::new(json_body.to_string());
-                    response.set_sized_body(cursor.get_ref().len(), cursor);
-                } else {
-                    // Handle the case where the body is not a Data body
-                    let error_message = "Invalid body type";
-                    let json_body = json!({
-                        "data": null,
-                        "status": Status::BadRequest.code,
-                        "message": error_message
-                    });
-                    let cursor = Cursor::new(json_body.to_string());
-                    response.set_sized_body(cursor.get_ref().len(), cursor);
+                match body.into() {
+                    Some(data) => {
+                        data.read_to_end(&mut body_bytes).await.unwrap();
+                        let body_str = String::from_utf8(body_bytes).unwrap();
+                        if body_str.contains("404: Not Found") {
+                            //404 Not Found
+                            return;
+                        }
+                        let body_json: serde_json::Value = serde_json::from_str(&body_str).unwrap();
+                        let json_body = json!({ "data": body_json, "status": Status::Ok.code, "message": "操作成功" });
+                        let cursor = Cursor::new(json_body.to_string());
+                        response.set_sized_body(cursor.get_ref().len(), cursor);
+                    }
+                    None => {
+                        let error_message = "Invalid body type";
+                        let json_body = json!({
+                            "data": null,
+                            "status": Status::BadRequest.code,
+                            "message": error_message
+                        });
+                        let cursor = Cursor::new(json_body.to_string());
+                        response.set_sized_body(cursor.get_ref().len(), cursor);
+                    }
                 }
             }
         }
